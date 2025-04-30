798b1e951205c05412cea499b5a1e9a4460441e3
Marc Grupe <marc.grupe@gmail.com>
2025-04-30 19:19:57 -0400

    updated logout navigation and notes

--- src/components/dashboards/UserScreen.jsx
+++ src/components/dashboards/UserScreen.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function ServiceDashboard() {
+export default function UserScreen() {
   return (
     <div>
       <div className="grid grid-cols-3 h-full w-full">
         <div className="bg-green-400 border row-span-2">1</div>
         <div className="bg-blue-400 border h-100 relative">2</div>
-        <div className="bg-amber-400 border row-span-2 ">3</div>
+        <div className="bg-amber-400 border row-span-2 ">User Dashboard</div>
         <div className="bg-teal-400 border  h-100">4</div>
         <div className="bg-sky-400 border h-100">5</div>
         <div className="bg-red-400 border h-100">6</div>
