207ff355f9c27cd3797ded6ba597ece4245b727e
Marc Grupe <marc.grupe@gmail.com>
2025-05-23 19:14:09 -0400

    working on an issue with  user selecting a service

--- src/features/plans/Services.jsx
+++ src/features/plans/Services.jsx
@@ -68,7 +68,7 @@ export default function Services() {
       let serviceToUpdate = userListOfServices.find((obj) =>
         Object.values(obj).includes(code)
       );
-      console.log('after find', serviceToUpdate);
+      console.log('after find', typeof serviceToUpdate);
     } 
 
     /* if user has existing services and one matched then update else create new entry.  This is for the 3 options of 'Cut Lawn' */
